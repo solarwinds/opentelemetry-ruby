c741105b9e96ffe03c0d0ca71f6cec5d63371494
xuan-cao-swi <xuan.cao@solarwinds.com>
2024-01-02 15:14:54 -0500

    NH-55892: use set -e to exit

--- .github/workflows/script/sw_build_and_push_gem.sh
+++ .github/workflows/script/sw_build_and_push_gem.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 GEM_NAME="$1"
 
 case $GEM_NAME in
@@ -34,9 +36,4 @@ gem_version=$(grep -E "VERSION\s*=\s*'[^']+'" "$found_file" | awk -F "'" '{print
 gem build "opentelemetry-$GEM_NAME.gemspec"
 gem push --key github --host https://rubygems.pkg.github.com/solarwinds "opentelemetry-$GEM_NAME-$gem_version.gem"
 
-if [ $? -eq 0 ]; then
-  echo "Finished"
-else
-  echo "Failed"
-  exit 1
-fi
+echo "Finished"
